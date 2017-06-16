737dbec047ba4d20ff6974fc3a1cd6f5d1c7f63a
Olga Krasova <Olga Krasova>
2017-06-15 21:23:14 -0400

    Implementing a Built-in Intent

--- src/main/java/wundershoppinglist/WunderShoppingListSpeechlet.java
+++ src/main/java/wundershoppinglist/WunderShoppingListSpeechlet.java
@@ -3,8 +3,7 @@ package wundershoppinglist;
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.*;
-import com.amazon.speech.ui.PlainTextOutputSpeech;
-import com.amazon.speech.ui.SimpleCard;
+import com.amazon.speech.ui.*;
 
 /**
  * Created by osamo on 6/13/2017.
@@ -27,9 +26,46 @@ public class WunderShoppingListSpeechlet implements Speechlet {
 
         if ("AddIntent".equals(intentName)) {
             return handleAddIntentRequest(intent, session);
+        } else if ("AMAZON.HelpIntent".equals(intentName)) {
+            return handleHelpRequest();
+        } else if ("AMAZON.StopIntent".equals(intentName)) {
+            return handleStopIntent();
+        } else if ("AMAZON.CancelIntent".equals(intentName)) {
+            return handleCancelIntent();
         } else {
             throw new SpeechletException("Invalid Intent");
         }
+    }
+
+    private SpeechletResponse handleHelpRequest() {
+        String repromptText = "What do you want to add to shopping wunderlist?";
+        String speechOutput = repromptText;
+
+        return newAskResponse(speechOutput, repromptText);
+    }
+
+    private SpeechletResponse handleStopIntent() {
+        PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+        outputSpeech.setText("Goodbye");
+        return SpeechletResponse.newTellResponse(outputSpeech);
+    }
+
+    private SpeechletResponse handleCancelIntent() {
+        PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+        outputSpeech.setText("Goodbye");
+        return SpeechletResponse.newTellResponse(outputSpeech);
+    }
+
+    private SpeechletResponse handleAddIntentRequest(Intent intent, Session session) {
+        Slot itemSlot = intent.getSlot("item");
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Shopping Wunderlist");
+        card.setContent("test");
+        // Create the plain text output
+        PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+        outputSpeech.setText("test");
+        return SpeechletResponse.newTellResponse(outputSpeech, card);
     }
 
     @Override
@@ -37,15 +73,48 @@ public class WunderShoppingListSpeechlet implements Speechlet {
 
     }
 
-    private SpeechletResponse handleAddIntentRequest(Intent intent, Session session) {
-        Slot itemSlot = intent.getSlot("item");
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("Tide Pooler");
-        card.setContent("test");
-        // Create the plain text output
-        PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-        outputSpeech.setText("test");
-        return SpeechletResponse.newTellResponse(outputSpeech, card);
+    /**
+     * Wrapper for creating the Ask response from the input strings with
+     * plain text output and reprompt speeches.
+     *
+     * @param stringOutput the output to be spoken
+     * @param repromptText the reprompt for if the user doesn't reply or is misunderstood.
+     * @return SpeechletResponse the speechlet response
+     */
+    private SpeechletResponse newAskResponse(String stringOutput, String repromptText) {
+        return newAskResponse(stringOutput, false, repromptText, false);
+    }
+
+    /**
+     * Wrapper for creating the Ask response from the input strings.
+     *
+     * @param stringOutput   the output to be spoken
+     * @param isOutputSsml   whether the output text is of type SSML
+     * @param repromptText   the reprompt for if the user doesn't reply or is misunderstood.
+     * @param isRepromptSsml whether the reprompt text is of type SSML
+     * @return SpeechletResponse the speechlet response
+     */
+    private SpeechletResponse newAskResponse(String stringOutput, boolean isOutputSsml,
+                                             String repromptText, boolean isRepromptSsml) {
+        OutputSpeech outputSpeech, repromptOutputSpeech;
+        if (isOutputSsml) {
+            outputSpeech = new SsmlOutputSpeech();
+            ((SsmlOutputSpeech) outputSpeech).setSsml(stringOutput);
+        } else {
+            outputSpeech = new PlainTextOutputSpeech();
+            ((PlainTextOutputSpeech) outputSpeech).setText(stringOutput);
+        }
+
+        if (isRepromptSsml) {
+            repromptOutputSpeech = new SsmlOutputSpeech();
+            ((SsmlOutputSpeech) repromptOutputSpeech).setSsml(stringOutput);
+        } else {
+            repromptOutputSpeech = new PlainTextOutputSpeech();
+            ((PlainTextOutputSpeech) repromptOutputSpeech).setText(repromptText);
+        }
+
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(repromptOutputSpeech);
+        return SpeechletResponse.newAskResponse(outputSpeech, reprompt);
     }
 }
